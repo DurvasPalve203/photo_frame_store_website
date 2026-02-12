54a9ea32b75c5a4adda9f1673d6fa389d5c4bf9d
Durvas Palve <durvaspalve18@gmail.com>
2026-02-12 16:33:06 +0530

    Add routing and basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -29,4 +29,5 @@ function Navbar() {
   );
 }
 
+
 export default Navbar;
