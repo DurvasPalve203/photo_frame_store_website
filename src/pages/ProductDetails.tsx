8cdb26af12e194880c26505715aee04a6c6c00bb
Durvas Palve <durvaspalve18@gmail.com>
2026-02-25 09:43:11 +0530

    Refactor pages to use shared product data

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { products } from "../lib/products";
 
 const products = [
   {
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,27 +1,5 @@
-import ProductCard, { Product } from "../components/products/ProductCard";
-
-const products: Product[] = [
-  {
-    id: "1",
-    name: "Classic Wooden Frame",
-    price: 449,
-    originalPrice: 499,
-    image: "https://via.placeholder.com/300x300",
-  },
-  {
-    id: "2",
-    name: "Premium Gold Frame",
-    price: 799,
-    originalPrice: 899,
-    image: "https://via.placeholder.com/300x300",
-  },
-  {
-    id: "3",
-    name: "Modern Black Frame",
-    price: 699,
-    image: "https://via.placeholder.com/300x300",
-  },
-];
+import ProductCard from "../components/products/ProductCard";
+import { products } from "../lib/products";
 
 function Products() {
   return (
